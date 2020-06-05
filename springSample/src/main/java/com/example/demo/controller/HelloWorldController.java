6c5be7c0115c9617796c9af06b0391b5d6c56989
81801 <81801@192.168.1.4>
2020-06-05 15:56:02 +0900

    Spring Boot - Hello World

---
+++ springSample/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String helloWorld(Model model) {
+        model.addAttribute("message", "Hello World!!");
+        return "index";
+    }
+
+    /**
+     * RequestMappingのテスト http://localhost:8080/alter にアクセス
+     */
+    @RequestMapping(value = "/alter", method = RequestMethod.GET)
+    public String alteredHelloWorld(Model model) {
+        model.addAttribute("message", "Altered Hello World!!");
+        return "index";
+    }
+
+}
